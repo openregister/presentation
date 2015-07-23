c1b34b55c63869e9ae7fdff0dcda0cd690991e44
omsharma <foromsharma@gmail.com>
2015-07-23 14:55:40 +0100

    Using HK2 to provide dependency injection so that registerFactory can be injected in to resources Register objects are created for every different register name and stored in in-memory cache(hashmap). all further calls for same register are using the existing register object. Right now there will a separate managed connection pool for every register which we might not want. More research required to make it work in one pool only. kafka consumer is detached and the test is ignored for now.

--- src/main/java/uk/gov/register/presentation/resource/ResourceBase.java
+++ src/main/java/uk/gov/register/presentation/resource/ResourceBase.java
@@ -1,21 +1,22 @@
 package uk.gov.register.presentation.resource;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Context;
+import uk.gov.register.presentation.app.Register;
+import uk.gov.register.presentation.app.RegisterProvider;
+import uk.gov.register.presentation.dao.RecentEntryIndexQueryDAO;
 
 public abstract class ResourceBase {
     public static final int ENTRY_LIMIT = 100;
-    @Context
-    protected HttpServletRequest httpServletRequest;
+
+    protected final Register register;
+    protected final RecentEntryIndexQueryDAO queryDAO;
+
+    public ResourceBase(RegisterProvider registerProvider) {
+        this.register = registerProvider.provide();
+        this.queryDAO = register.getQueryDAO();
+    }
 
     protected String getRegisterPrimaryKey() {
-        String host = httpServletRequest.getHeader("Host");
-
-        if (host.contains(".")) {
-            return host.substring(0, host.indexOf("."));
-        } else {
-            return host;
-        }
+        return register.getName();
     }
 }
 
