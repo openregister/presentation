aaa76f9f9283c482172e0d85d041a3c37344b7cb
Andrew Gorton <a.gorton@kainos.com>
2016-01-12 09:30:52 +0000

    Provided a means to get a urlencoded hash so that links using the hash are not broken due to the presence of special chars in the hash.

--- src/main/java/uk/gov/register/presentation/Version.java
+++ src/main/java/uk/gov/register/presentation/Version.java
@@ -1,8 +1,16 @@
 package uk.gov.register.presentation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class Version {
+    private final Logger LOGGER = LoggerFactory.getLogger(Version.class);
+
     @JsonProperty
     public final String hash;
     @JsonProperty("serial-number")
@@ -11,5 +19,14 @@ public class Version {
     public Version(int serialNumber, String hash) {
         this.hash = hash;
         this.serialNumber = serialNumber;
+    }
+
+    public String getUrlEncodedHash() {
+        try {
+            return URLEncoder.encode(hash, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error(e.toString());
+            throw new RuntimeException(e);
+        }
     }
 }
