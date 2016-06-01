91b31ea3b3fb29cb305c2036e16489e3e2b0e8c2
michaelabroschomb <michaela.broschomb@digital.cabinet-office.gov.uk>
2016-06-01 15:55:47 +0100

    Fix audit and consistency proofs to use iterator in transaction

--- src/main/java/uk/gov/register/presentation/resource/VerifiableLogResource.java
+++ src/main/java/uk/gov/register/presentation/resource/VerifiableLogResource.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import org.apache.commons.lang3.StringUtils;
 import uk.gov.register.presentation.dao.Entry;
 import uk.gov.register.presentation.dao.EntryDAO;
 import uk.gov.verifiablelog.merkletree.MerkleTree;
@@ -12,6 +13,7 @@ import uk.gov.verifiablelog.merkletree.MerkleTree;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.DatatypeConverter;
@@ -38,40 +40,48 @@ public class VerifiableLogResource {
         try {
             // need to use a transaction to enable use of a cursor
             entryDAO.begin();
-            int totalEntries = entryDAO.getTotalEntries();
-            return rootHash(totalEntries,
-                    Iterables.transform(
-                            () -> entryDAO.entriesIterator(1, totalEntries + 1),
-                            this::bytesFromEntry));
+            MerkleTree merkleTree = merkleTree();
+            return bytesToString(merkleTree.currentRoot());
         } finally {
             entryDAO.rollback();
         }
     }
 
-    public String rootHash(int iteratorSize, Iterable<byte[]> leafIterator) throws NoSuchAlgorithmException {
-        MerkleTree merkleTree = new MerkleTree(MessageDigest.getInstance("SHA-256"),
-                null,
-                () -> iteratorSize,
-                leafIterator
-        );
-        return bytesToString(merkleTree.currentRoot());
+    @GET
+    @Path("/audit/{snapshot}/{entry-number}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAuditPath(@PathParam("entry-number") int entryNumber, @PathParam("snapshot") int snapshot) throws NoSuchAlgorithmException {
+        try {
+            entryDAO.begin();
+            MerkleTree merkleTree = merkleTree();
+            List<String> path = bytesToString(merkleTree.pathToRootAtSnapshot(entryNumber - 1, snapshot));
+            return StringUtils.join(path, ",");
+        } finally {
+            entryDAO.rollback();
+        }
     }
 
-//    @GET
-//    @Path("/audit/{snapshot}/{entry-number}")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public String getAuditPath(@PathParam("entry-number") int entryNumber, @PathParam("snapshot") int snapshot) throws NoSuchAlgorithmException {
-//        List<String> path = bytesToString(verifiableLog.pathToRootAtSnapshot(entryNumber - 1, snapshot));
-//        return StringUtils.join(path, ",");
-//    }
-//
-//    @GET
-//    @Path("/consistency/{m}/{n}")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public String getConsistencySet(@PathParam("m") int m, @PathParam("n") int n) throws NoSuchAlgorithmException {
-//        List<String> path = bytesToString(verifiableLog.snapshotConsistency(m, n));
-//        return StringUtils.join(path, ",");
-//    }
+    @GET
+    @Path("/consistency/{m}/{n}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getConsistencySet(@PathParam("m") int m, @PathParam("n") int n) throws NoSuchAlgorithmException {
+        try {
+            entryDAO.begin();
+            MerkleTree merkleTree = merkleTree();
+            List<String> path = bytesToString(merkleTree.snapshotConsistency(m, n));
+            return StringUtils.join(path, ",");
+        } finally {
+            entryDAO.rollback();
+        }
+    }
+
+    private MerkleTree merkleTree() throws NoSuchAlgorithmException {
+        return new MerkleTree(MessageDigest.getInstance("SHA-256"),
+                (i, j) -> Iterators.transform(
+                        entryDAO.entriesIterator(i + 1, j + 1),
+                        this::bytesFromEntry),
+                () -> entryDAO.getTotalEntries());
+    }
 
     private byte[] bytesFromEntry(Entry value) {
         try {
